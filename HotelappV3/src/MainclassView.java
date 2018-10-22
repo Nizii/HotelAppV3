6931ae9fdaf76a6fe2dbaaa0f81507b160d70113
bachelor <bachelor@192.168.1.22>
2018-10-22 17:03:00 +0200

    Anpassung Kommentar

--- HotelappV3/src/MainclassView.java
+++ HotelappV3/src/MainclassView.java
@@ -1,3 +1,21 @@
+/**
+* Herr Wanner schön das Sie sich die Zeit genommen haben rein zu schauen.
+* 
+* Dieses CRM System entwickle ich für die Eltern meiner Freundin die in Thailand leben und eine kleine Pension mit 30 Zimmern betreiben.
+* Mit diesem CRM System können Sie ihre Finanzen, Buchungen, Kunden und die Liegenschaft an sich verwalten. Sie können Quittungen, Buchungen und Kunden erfassen, bearbeiten oder löschen.
+* Zudem können Sie Räume erstellen bearbeiten oder löschen. Quittungen werden über Excel geöffnet. das habe ich mit Woorkbook gemacht.
+* Die Datenbankverbindung wird mit dem JDBC Treiber hergestellt aktuell local auf meinem PC.
+* Die MainclassView ist die Hauptklasse hier startet die App.
+* Hier in der MainclassView befindet sich das gesamte Frontend (Tabs, Tablellen Buttons Textfields usw.), diese kommuniziert über Funktionen mit den ModlerKlassen und diese Wiederum mit der Klasse Controller für die SQL Anfragen.
+* 
+* Leider habe ich noch keine Dokumentation erstellt. Ich würde ihnen aber gerne bei Interesse das System live zeigen im Zulassungsgespräch.
+*
+* Bitte entschuldigen Sie meine Rechtsschreibung in den Kommentaren
+*
+* @author  Özdemir Nizam
+*/
+
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
@@ -28,23 +46,6 @@ import java.awt.SystemColor;
 import javax.swing.border.MatteBorder;
 import javax.swing.JScrollPane;
 import java.awt.Window.Type;
-
-/**
-* Herr Wanner schön das Sie sich die Zeit genommen haben rein zu schauen.
-* 
-* Dieses CRM System entwickle ich für die Eltern meiner Freundin die in Thailand leben und eine kleine Pension mit 30 Zimmern betreiben.
-* Mit diesem CRM System können Sie ihre Finanzen, Buchungen, Kunden und die Liegenschaft an sich verwalten. Sie können Quittungen, Buchungen und Kunden erfassen, bearbeiten oder löschen.
-* Zudem können Sie Räume erstellen bearbeiten oder löschen. Quittungen werden über Excel geöffnet. das habe ich mit Woorkbook gemacht.
-* Die Datenbankverbindung wird mit dem JDBC Treiber hergestellt aktuell local auf meinem PC.
-* Die MainclassView ist die Hauptklasse hier startet die App.
-* Hier in der MainclassView befindet sich das gesamte Frontend (Tabs, Tablellen Buttons Textfields usw.), diese kommuniziert über Funktionen mit den ModlerKlassen und diese Wiederum mit der Klasse Controller für die SQL Anfragen.
-* 
-* Leider habe ich noch keine Dokumentation erstellt. Ich würde ihnen aber gerne bei Interesse das System live zeigen im Zulassungsgespräch.
-*
-* Bitte entschuldigen Sie meine Rechtsschreibung in den Kommentaren
-*
-* @author  Özdemir Nizam
-*/
 
 public class MainclassView {
 
